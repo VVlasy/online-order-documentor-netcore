531f6bc5f579064977bb1b58258462cf5306efb5
Victor Vlasák <victor@vlasak.me>
2021-06-01 00:06:58 +0200

    add auto generation for id

--- online-order-documentor/Models/FeedFilter.cs
+++ online-order-documentor/Models/FeedFilter.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,7 @@ namespace online_order_documentor_netcore.Models
     public class FeedFilter
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
     }
 }
