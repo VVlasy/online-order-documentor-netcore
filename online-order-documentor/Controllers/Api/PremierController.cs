8f72dcfc280b76c9f56736129e6a26f5e5a84c34
Victor Vlasák <victor@vlasak.me>
2021-06-29 21:46:40 +0200

    add czc feed

--- online-order-documentor/Controllers/Api/PremierController.cs
+++ online-order-documentor/Controllers/Api/PremierController.cs
@@ -39,6 +39,22 @@ namespace online_order_documentor_netcore.Controllers.Api
                     s = s.ModifyTag("PURCHASEPRICE", (s) => s.Replace(" ", string.Empty));
                     s = s.ModifyTag("PRICE_NO_VAT", (s) => s.Replace(" ", string.Empty));
                 }
+                else if (filename == "czcfeed")
+                {
+                    s = s.Replace("VFPData", "SHOP");
+                    s = s.Replace("que_txt", "SHOPITEM");
+
+                    s = s.Replace("sloupec01", "ID");
+                    s = s.Replace("sloupec02", "NAME");
+                    s = s.Replace("sloupec03", "EAN");
+                    s = s.Replace("sloupec04", "STOCK");
+                    s = s.Replace("sloupec05", "PURCHASEPRICE");
+                    s = s.Replace("sloupec06", "PRICE_NO_VAT");
+                    s = s.Replace("sloupec07", "WEIGHT");
+
+                    s = s.ModifyTag("PURCHASEPRICE", (s) => s.Replace(" ", string.Empty).Replace(',', '.'));
+                    s = s.ModifyTag("PRICE_NO_VAT", (s) => s.Replace(" ", string.Empty).Replace(',', '.'));
+                }
 
                 return this.Xml(s);
             }
