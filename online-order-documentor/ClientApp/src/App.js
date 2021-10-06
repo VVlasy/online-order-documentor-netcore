ed3a63830a6ddf0a6e55cd3f8232ea974b56fc30
Victor Vlasák <victor@vlasak.me>
2021-10-06 19:57:38 +0200

    fix(clientapp): add router and cleanup behavior

--- online-order-documentor/ClientApp/src/App.js
+++ online-order-documentor/ClientApp/src/App.js
@@ -3,64 +3,29 @@ import React from 'react';
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
-import axios from 'axios';
-
-import { Transition } from 'semantic-ui-react';
-
-import eventBus from "./helpers/EventBus.js";
-import { version } from '../package.json';
 import PhotoDocumentor from './PhotoDocumentor';
+import Version from './components/Version';
+
+
+import ErrorNotFound from './components/ErrorNotFound';
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 export default class App extends React.Component {
     constructor(props) {
         super(props);
         this.cameraRef = React.createRef();
         this.state = {
-            width: window.innerWidth,
-            showSuccess: false,
-            showError: false
+            width: window.innerWidth
         };
 
         window.eventsHooked = false;
     }
-
-    componentDidMount() {
-        eventBus.on("showError", (text) => {
-            this.setState({ showError: true });
-        });
-        eventBus.on("showSuccess", (text) => {
-            this.setState({ showSuccess: true });
-        });
-
-        this.checkUpdates();
-    }
-
-    onSuccessAnimationFinish() {
-        setTimeout(() => this.setState({ showSuccess: false }), 5000);
-    }
-
-    onErrorAnimationFinish() {
-        setTimeout(() => this.setState({ showError: false }), 5000);
-    }
-
-    checkUpdates() {
-        axios.get('/api/ClientApp/version').then((response) => {
-            if (response.data.version === undefined) {
-                console.error("Api result invalid");
-                return;
-            }
-
-            if (version !== response.data.version) {
-                console.log("Versions dont match! Updating...");
-                this.forceSWupdate();
-            } else {
-                console.log("Version is up to date");
-            }
-        }).catch((err) => {
-            console.log("Check update failed");
-        });
-    }
-
 
     forceSWupdate() {
         if ('serviceWorker' in navigator) {
@@ -75,33 +40,22 @@ export default class App extends React.Component {
 
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <PhotoDocumentor />
+            <Router>
+                <div className="App">
+                    <header className="App-header">
+                        <Switch>
+                            <Route exact path="/">
+                                <PhotoDocumentor />
+                            </Route>
+                            <Route path='*' exact={true}>
+                                <ErrorNotFound />
+                            </Route>
+                        </Switch>
 
-                    <div className='version-text'>
-                        Verze: {version}
-
-                        <Transition visible={this.state.showSuccess}
-                            onShow={this.onSuccessAnimationFinish.bind(this)}
-                            duration={250}
-                            animation='slide up'>
-                            <div className='notification successNotification'>
-                                <p className='notificationText'>Fotka byla nahrána</p>
-                            </div>
-                        </Transition>
-
-                        <Transition visible={this.state.showError}
-                            onShow={this.onErrorAnimationFinish.bind(this)}
-                            duration={250}
-                            animation='slide up'>
-                            <div className='notification errorNotification'>
-                                <p className='notificationText'>Chyba při nahrávání fotky</p>
-                            </div>
-                        </Transition>
-                    </div>
-                </header>
-            </div>
+                        <Version />
+                    </header>
+                </div>
+            </Router>
         );
     }
 }
